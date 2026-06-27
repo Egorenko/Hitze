80fe4dc12ec3026cdd1df1aa7c7f9df07a10c4ad
Julius Knust <julius.knust@gmail.com>
2026-06-27 23:39:29 +0200

    card(exept swipe) work completley

--- 20260626_game_jam/scripts/card_new/card_stack.gd
+++ 20260626_game_jam/scripts/card_new/card_stack.gd
@@ -15,13 +15,17 @@ func is_empty() -> bool:
 
 func choose_card() -> card_new:
 	if not all_same_probpability:
+		print("nsp")
 		for el in cards:
 			#[0..100]
-			if randf() * 100 >= el.propability:
+			var test = (randf() * 100)
+			print(test, " / ", el.propability)
+			if test <= el.propability:
 				el.amount -= 1
 				if not legal_card(el):
 					cards.erase(el)
 				return el
+	print("sp")
 	#if all same or (somehow) nothing in first try
 	var rand = randi_range(0, cards.size()-1)
 	var chosen_card = cards[rand]
